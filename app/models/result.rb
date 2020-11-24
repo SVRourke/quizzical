e17a0aa131ea3544d302f547fe87372e52244e9b
SVRourke <samuel@rourke.tech>
2020-11-24 14:15:38 -0500

    Added #grade to Result- returns the percentage grade

--- app/models/result.rb
+++ app/models/result.rb
@@ -4,5 +4,10 @@ class Result < ApplicationRecord
     
     has_many :user_answers
     has_many :answers, through: :user_answers
+
+    def grade
+        correct_answers = answers.where(correct_answer: true).count
+        return (correct_answers.to_f / answers.count.to_f) * 100
+    end
  
 end
