faa6af77a2fb53d948344b6a779a95641eeac2b5
SVRourke <samuel@rourke.tech>
2020-11-24 10:44:35 -0500

    Created User model and migration

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+    # Teacher Relationships
+    has_many :group_admins
+    has_many :taught_classes, through: :group_admins, source: :group
+    has_many :students, through: :taught_classes, source: :students
+    has_many :quizzes
+    
+    # Student Relationships
+    has_and_belongs_to_many :enrolled_classes, 
+        :class_name => 'Group', 
+        :join_table => 'groups_users', 
+        :foreign_key => 'group_id', 
+        :association_foreign_key => 'user_id'
+
+    has_many :assigned_quizzes, through: :enrolled_classes, source: :quizzes
+    has_many :teachers, through: :enrolled_classes, source: :teacher
+    has_many :results
+
+end
---
+++ db/migrate/20201124154231_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.boolean :teacher
+      t.timestamps
+    end
+  end
+end
