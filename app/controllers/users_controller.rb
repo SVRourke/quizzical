87b8070551d495adec5e68de5b8677003436412d
SVRourke <samuel@rourke.tech>
2020-11-25 15:50:21 -0500

    Added redirect to dashboard on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
         session[:user_id] = @user.id
-        # REDIRECT TO DASH
+        redirect_to dashboards_path
     else
         redirect_back
     end
