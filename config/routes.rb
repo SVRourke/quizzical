f4433be90e24807a997a5ff40232224528f196e2
SVRourke <samuel@rourke.tech>
2020-11-25 09:32:09 -0500

    updated http verb in logout

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  post 'logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
 
 
 end
