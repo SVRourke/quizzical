8ec12dbdb57c92b7bd0ae95b503b8212666d2b8a
SVRourke <samuel@rourke.tech>
2020-11-25 13:48:18 -0500

    added user registration functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,36 @@
 class UsersController < ApplicationController
-    def new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+        session[:user_id] = @user.id
+        # REDIRECT TO DASH
+    else
+        redirect_back
     end
-    
-    def create
-    end
-    
-    def edit
-    end
-    
-    def update
-    end
-    
-    def destroy
-    end
-    
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :teacher
+    )
+  end
 end
