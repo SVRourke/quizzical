55bde327eb9a0005a899655a96414ca86cb7ac94
SVRourke <samuel@rourke.tech>
2020-11-25 09:32:30 -0500

    fleshed out create and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,18 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(email: params[:email])
+        if @user.valid? && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            # ADD REDIRECT TO USER DASHBOARD
+        else
+            redirect_back(fallback_location: root_path)
+        end
     end
 
     def destroy
+        session.clear
+        redirect_to root_path
     end
 
 end
