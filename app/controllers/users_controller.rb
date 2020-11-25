9fd4ab31da3867141dac7761a1b75d4cfb5bb8d6
SVRourke <samuel@rourke.tech>
2020-11-25 09:34:25 -0500

    Created Users Controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+    def new
+    end
+    
+    def create
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+end
