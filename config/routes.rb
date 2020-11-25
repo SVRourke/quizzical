8e64ea4670ff00dd63370d5c7a3eddf9218ea679
SVRourke <samuel@rourke.tech>
2020-11-24 20:53:23 -0500

    Added root route, static controller and welcome view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "static#welcome"
+  
+  
+  
 end
+
+
+# AS A STUDENT
+  # Dashboard "/students/3/home"
+  # Take a quiz "/group/4/quiz/10/submit"
+  # Join a group "group/4/join"
+
+  # resource :quiz, except: [:update] do
+  #   member do
+  #     get 'submit'
+  #     post 'submit', action: :submit_quiz
+  #   end
+  # end
+  # resources :groups, only: [:show] do
+  #   resource :quiz, only: [:show] do
+  #     member do
+  #       get 'submit'
+  #       post 'submit', action: :submit_quiz
+  #     end
+  #   end
+  # end
+
+  # resource :quiz, only: [:new, :create, :update]
