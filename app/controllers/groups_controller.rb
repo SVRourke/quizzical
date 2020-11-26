64c3cc8c63615ba0198773d164c194e92a327527
SVRourke <samuel@rourke.tech>
2020-11-26 00:30:46 -0500

    moved enrollement mgmt to enrollments controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,15 +21,4 @@ class GroupsController < ApplicationController
     def destroy
     end
     
-    # maybe moved to enrollment controller??
-    def submit_enrollment
-        # can be refactored by scoping groups
-        if current_user.enrolled_classes.exists?(params[:id])
-            # add an alert
-            redirect_to groups_path
-        else
-            current_user.enrolled_classes << Group.find(params[:id])
-            redirect_to dashboards_path
-        end
-    end
 end
