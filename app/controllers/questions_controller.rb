da2d1e722929bf38773bfd3c279d892e4c18c282
SVRourke <samuel@rourke.tech>
2020-11-27 18:09:11 -0500

    added #question_params

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,10 +8,16 @@ class QuestionsController < ApplicationController
     end
 
     def create
-        5.times {puts ""}
-        puts params
-        5.times {puts ""}
+        5.times {puts "+"}
+        puts question_params
+        5.times {puts "+"}
         redirect_back fallback_location: '/'
+    end
+
+    private
+
+    def question_params
+        params.require(:question).permit(:quiz_id, :content, :answers_attributes => {})
     end
 
 end
