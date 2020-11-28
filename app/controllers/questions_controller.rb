11fad117b0cd87652ab03b27e843986416f6083b
SVRourke <samuel@rourke.tech>
2020-11-27 19:33:04 -0500

    added destroy action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,6 +12,11 @@ class QuestionsController < ApplicationController
         redirect_back fallback_location: '/'
     end
 
+    def destroy
+        Question.find(params[:id]).destroy
+        redirect_to new_quiz_question_path(params[:quiz_id])
+    end
+
     private
 
     def question_params
