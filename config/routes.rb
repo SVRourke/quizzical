7638ca95a16e875b0c413bdc327f48ca0a8903e4
SVRourke <samuel@rourke.tech>
2020-11-27 19:30:22 -0500

    updated questions routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,29 +22,7 @@ Rails.application.routes.draw do
     member do
       post 'publish', to: 'quizzes#publish'
     end
-    resources :questions
+    resources :questions, only: [:new, :create, :destroy]
   end
   
 end
-
-# AS A STUDENT
-  # Dashboard "/students/3/home"
-  # Take a quiz "/group/4/quiz/10/submit"
-  # Join a group "group/4/join"
-
-  # resource :quiz, except: [:update] do
-  #   member do
-  #     get 'submit'
-  #     post 'submit', action: :submit_quiz
-  #   end
-  # end
-  # resources :groups, only: [:show] do
-  #   resource :quiz, only: [:show] do
-  #     member do
-  #       get 'submit'
-  #       post 'submit', action: :submit_quiz
-  #     end
-  #   end
-  # end
-
-  # resource :quiz, only: [:new, :create, :update]
