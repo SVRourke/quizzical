6983bf3a5b9bcdfcc5f60c742b4eb6ff5067ef20
SVRourke <samuel@rourke.tech>
2020-11-28 08:14:40 -0500

    added accept nest attrs

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,9 +1,11 @@
 class Result < ApplicationRecord
+    
     belongs_to :quiz
     belongs_to :user
     
     has_many :user_answers
     has_many :answers, through: :user_answers
+    accepts_nested_attributes_for :answers
 
     def grade
         correct_answers = answers.where(correct_answer: true).count
