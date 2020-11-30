b399536b4bb02fe0eb7ce250c536b5b6c55adfbb
SVRourke <samuel@rourke.tech>
2020-11-30 17:49:37 -0500

    restructured relationsships for answered_questions

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,15 +1,11 @@
 class Result < ApplicationRecord
-    
     belongs_to :quiz
     belongs_to :user
-    
-    has_many :user_answers
-    has_many :answers, through: :user_answers
-    accepts_nested_attributes_for :answers
 
-    def grade
-        correct_answers = answers.where(correct_answer: true).count
-        return (correct_answers.to_f / answers.count.to_f) * 100
-    end
+    has_many :answered_questions
+    has_many :questions, through: :answered_questions
+    has_many :answers, through: :answered_questions
+
+    accepts_nested_attributes_for :answered_questions
  
 end
