f59d396c9df01eb024465075e8e46788d860fae4
SVRourke <samuel@rourke.tech>
2020-12-01 10:42:01 -0500

    Added helper to return a word for the time of day

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def colloquial_time_of_day
+        case Time.now.strftime("%H").to_i
+        when 1..11
+            "morning"
+        when 12..4
+            "afternoon"
+        when 4..0
+            "evening"
+        else
+            'day'
+        end
+    end
 end
