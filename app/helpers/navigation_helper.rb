0e97e6bd2c1229d9c12cd4e3d14955db5b8b21ff
SVRourke <samuel@rourke.tech>
2020-12-01 13:58:45 -0500

    moved #ctod to navigation helper

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,4 +2,17 @@ module NavigationHelper
     def user_greeting(user)
         "Good #{colloquial_time_of_day}, #{user.first_name}"   
     end
+
+    def colloquial_time_of_day
+        case Time.now.strftime("%H").to_i
+        when 1..11
+            "Morning"
+        when 12..16
+            "Afternoon"
+        when 16..24
+            "Evening"
+        else
+            'Day'
+        end
+    end
 end
