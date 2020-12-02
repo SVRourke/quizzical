ebcad583f2952c05e8ac0dbaeae2b80527482298
SVRourke <samuel@rourke.tech>
2020-12-02 13:29:03 -0500

    added method to check if a quiz has been completed by a given user

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,7 +1,14 @@
 class Quiz < ApplicationRecord
     scope :published, -> { where("published == true")}
+    # scope :completed
 
     has_many :questions
     belongs_to :group
     has_many :results
+
+    def completed_by(user)
+        results.any? do |r|
+            r.user == user
+        end
+    end
 end
