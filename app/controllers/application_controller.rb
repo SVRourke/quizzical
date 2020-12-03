88f205e5de1004f29a744f8f899c72c7c9f3e18b
SVRourke <samuel@rourke.tech>
2020-12-03 14:44:44 -0500

    added helper to check logged in status from controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
     def current_user
         @user ||= User.find(session[:user_id])
     end
+
+    def logged_in?
+        session[:user_id].present? && current_user.id == session[:user_id]
+    end
 end
