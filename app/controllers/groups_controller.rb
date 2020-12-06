78fc4892f314c51cc4c451ebced1e74e0ff99d3e
SVR <samuelvanrourke@gmail.com>
2020-12-06 18:55:36 -0500

    modified to comply with pundit auth

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,24 +1,27 @@
 class GroupsController < ApplicationController
+    after_action :verify_authorized, except: :index
     def index
         @groups = Group.all
     end
 
     def show
         @group= Group.find(params[:id])
+        authorize @group
 
         if current_user.teacher
             render :teacher_view_group
-        else
-            render :student_view_group
         end
     end
 
     def new
+        authorize Group, :new?
     end
 
     def create
-        @group = Group.create(group_params)
-        @group.teacher = current_user
+        authorize Group, :create?
+        @group = current_user.taught_classes.build(group_params)
+        @group.save()
+
         redirect_to group_path(@group)
     end
 
