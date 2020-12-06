69eb7f3fbe62e666752b8d88829f499d0428a791
SVR <samuelvanrourke@gmail.com>
2020-12-06 18:54:36 -0500

    created group policy

---
+++ app/policies/group_policy.rb
@@ -0,0 +1,21 @@
+class GroupPolicy < ApplicationPolicy
+  attr_reader :user, :group
+ 
+  def new?
+    user.teacher?
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.teacher?
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
