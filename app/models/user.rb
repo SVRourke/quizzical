907bf4b307c6ecb4b26f4be5382cec7a00c4c2a3
SVRourke <samuel@rourke.tech>
2020-12-06 09:35:31 -0500

    added some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
     has_secure_password
 
+    # VALIDATIONS
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    # email is present and unique and an email (can be simple format check)
+    validates :email, presence: true, uniqueness: true
+    
+    # password for presence, length and presence of special characters
+
     # Teacher Relationships
     has_many :group_admins
     has_many :taught_classes, through: :group_admins, source: :group
