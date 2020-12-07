681376f1d0d21a08d7b3d5353c9b5e1eed7259b9
SVRourke <samuel@rourke.tech>
2020-12-07 13:54:44 -0500

    added custom error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,16 @@ class User < ApplicationRecord
     has_secure_password
 
     # VALIDATIONS
-    validates :first_name, presence: true
-    validates :last_name, presence: true
+    validates :first_name, presence: true, message: 'Must have a first name!'
+    validates :last_name, presence: true, message: 'Must have a last name!'
     # email is present and unique and an email (can be simple format check)
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, message: 'Must have an email!'
+    validates :email, uniqueness: true, message: 'Email already in use!'
     
     # password for presence, length and presence of special characters
-    validates :password, presence: true, length: {minimum: 8}
-    
+    validates :password, presence: true, message: 'Must have a password!' 
+    validates :password, length: {minimum: 8}, message: 'Password must be at least 8 characters long.'
+
 
     # Teacher Relationships
     has_many :group_admins
