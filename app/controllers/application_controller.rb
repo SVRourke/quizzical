32dbb5c90dfb21c204cab50587e1a825af7c39f0
SVR <samuelvanrourke@gmail.com>
2020-12-06 21:20:11 -0500

    added a method to redirect if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
     include Pundit
 
+    def unauthorized_redirect
+        redirect_to :root if !logged_in?
+    end
+            
+
     def current_user
         @user ||= User.find(session[:user_id])
     end
