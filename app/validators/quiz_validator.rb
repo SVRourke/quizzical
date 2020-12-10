5a6f5ea6dcbe646c2c254f23bd350b231a7c7f38
SVRourke <samuel@rourke.tech>
2020-12-10 16:18:20 -0500

    fixed quiz validation

--- app/validators/quiz_validator.rb
+++ app/validators/quiz_validator.rb
@@ -1,6 +1,6 @@
 class QuizValidator < ActiveModel::Validator
     def validate(record)
-        if record.group.quizzes.collect {|q| q.name == record.name}
+        if record.group.quizzes.any? {|q| q.name == record.name}
             record.errors[:quiz] << "already exists for #{record.group.name}"
         end
     end
