d08383cdcb2ecc6d0d89339922b44f3cbaf2306d
SVRourke <samuel@rourke.tech>
2020-12-10 14:33:46 -0500

    finished validations for question model

--- app/validators/question_validator.rb
+++ app/validators/question_validator.rb
@@ -1,15 +1,27 @@
 class QuestionValidator < ActiveModel::Validator
     def validate(record)
         # may only have 1 right answer
-        if record.answers.count {|x| x.correct_answer} > 1
-            record.errors[:answers] << "A question may only have 1 correct answer!"
+        if record.answers.count {|a| a.correct_answer} > 1
+            record.errors[:question] << "may only have 1 correct answer!"
         end
-        15.times {puts record.answers.count}
-        # record.errors[:answers] << "Question must have 4 answers."
 
-        # content unique for a given quiz
+        # requires 1 correct answer
+        if record.answers.none? {|a| a.correct_answer}
+            record.errors[:question] << "must have 1 correct answer!"
+        end
+
+        # must have 4 answers
+        if record.answers.any? { |a| !a.content.present?}
+            record.errors[:question] << "must have 4 answers"
+        end
+
+        # answers must be unique
+        if !!record.answers.collect {|a| a.content}.uniq!
+            record.errors[:question] << "cannot have duplicate answers."
+        end
+        # question is unique for a given quiz
         if record.quiz.questions.any? {|q| q.content == record.content }
-            record.errors[:question] << "This question has already been added to this quiz."
+            record.errors[:question] << "has already been added to this quiz."
         end
 
     end
