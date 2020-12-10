6d86bd6bd6ea82e6f8523892cc0c80a31aeecca0
SVRourke <samuel@rourke.tech>
2020-12-10 14:36:19 -0500

    simplified authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(email: params[:email])
-        if @user.valid? && @user.authenticate(params[:password])
+        if @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to dashboards_path
         else
