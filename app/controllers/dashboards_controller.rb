985d182b50db6764d1d6403bac91742196393a4e
SVRourke <samuel@rourke.tech>
2020-12-11 10:13:37 -0500

    refactored

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,11 +2,12 @@ class DashboardsController < ApplicationController
     before_action :unauthorized_redirect
     def show
         @user = current_user
+
         if @user.teacher?
-            render :teacher
-        else
-            render :student
+            render :teacher and return
         end
+
+        render :student
     end
 
 end
