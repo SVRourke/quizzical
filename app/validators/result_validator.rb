26ab691e2a5225c7ea4527005b6bab21de4464f3
SVRourke <samuel@rourke.tech>
2020-12-11 15:54:01 -0500

    added check for result completion

--- app/validators/result_validator.rb
+++ app/validators/result_validator.rb
@@ -1,8 +1,11 @@
 class ResultValidator < ActiveModel::Validator
     def validate(record)
-        # ADD CHECK FOR ALL Questions
         if record.user.results.where(quiz: record.quiz).any?
             record.errors[:name] << "You have already completed this quiz."
-        end     
+        end
+
+        if record.answered_questions.any? {|aq| aq.answer_id.nil?}
+            record.errors[:questions] << "You must answer every question before submitting."
+        end
     end
 end
