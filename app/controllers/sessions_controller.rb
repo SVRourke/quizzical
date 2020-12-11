db9ce09a5018792b655ff0e3e185811b117aeeb8
SVRourke <samuel@rourke.tech>
2020-12-11 10:09:42 -0500

    slight refactor, needs validations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,17 @@
 class SessionsController < ApplicationController
     def new
     end
-
+    
+    # REFACTOR FOR ERROR MESSAGES
     def create
         @user = User.find_by(email: params[:email])
+
         if @user.authenticate(params[:password])
             session[:user_id] = @user.id
-            redirect_to dashboards_path
-        else
-            redirect_back(fallback_location: root_path)
+            redirect_to dashboards_path and return
         end
+
+        redirect_back(fallback_location: root_path)
     end
 
     def destroy
