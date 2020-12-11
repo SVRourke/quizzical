fbcb4aadff7b34ff6407d50032cf8a7b3cc5df26
SVRourke <samuel@rourke.tech>
2020-12-11 10:08:50 -0500

    slight refactor, needs more for validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,25 +3,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  # REFACTOR FOR VALIDATIONS
   def create
     @user = User.new(user_params)
     if @user.save
         session[:user_id] = @user.id
-        redirect_to dashboards_path
-    else
-      flash[:alert] = @user.errors.messages
+        redirect_to dashboards_path and return
+      end
       
-      redirect_back fallback_location: new_user_path()
-    end
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+    render :new
   end
 
   private
