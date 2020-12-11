ff59c7793d22919d0249e68220bb62d1d7638630
SVRourke <samuel@rourke.tech>
2020-12-11 15:53:40 -0500

    added optional flag for greater control of error messages

--- app/models/AnsweredQuestion.rb
+++ app/models/AnsweredQuestion.rb
@@ -1,7 +1,7 @@
 class AnsweredQuestion < ApplicationRecord
     belongs_to :result
     belongs_to :question
-    belongs_to :answer
+    belongs_to :answer, optional: true
 
     accepts_nested_attributes_for :answer
     accepts_nested_attributes_for :question
