0969d95f33238d738c1bcbca4a7b0fea629cc260
SVR <samuelvanrourke@gmail.com>
2020-12-13 12:48:31 -0500

    added action to login/create a user with google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
     def new
     end
     
-    # REFACTOR FOR ERROR MESSAGES
     def create
         @user = User.find_by(email: params[:email])
 
@@ -11,7 +10,7 @@ class SessionsController < ApplicationController
             redirect_to dashboards_path and return
         end
 
-        flash.notice = "Incorrect Login..."
+        flash.notice = "Unable to log in with provided credentials..."
         redirect_back fallback_location: :root
     end
 
@@ -21,17 +20,23 @@ class SessionsController < ApplicationController
     end
 
     def GoogleAuth
-        5.times {puts "+"}
-        # puts params[:teacher]
-        puts request.env["omniauth.auth"]
-        5.times {puts "+"}
+        if user = User.from_omniauth(auth, teacher_state)
+             session[:user_id] = user.id
+             redirect_to :dashboards and return
+        end
+    
+        flash.notice = 'Error logging in...'
+        render :new
+      end
+    
+    private
 
-        access_token = request.env["omniauth.auth"]
-        # user = User.from_omniauth(access_token)
-        # session[:user_id] = user.id
-        # user.google_token = access_token.credentials.token
-        # p user
-        # user.save
+    def teacher_state
+        request.env['omniauth.params']['teacher']
+    end
+    
+    def auth
+        request.env['omniauth.auth']['info']
     end
 
 end
