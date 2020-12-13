924c25c98c102ae95b7a8d998742f40e965cbd1d
SVRourke <samuel@rourke.tech>
2020-12-13 08:18:21 -0500

    added migration to make users compatible with oauth

---
+++ db/migrate/20201212122843_add_columns_to_users.rb
@@ -0,0 +1,6 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :google_token, :string
+    add_column :users, :google_refresh_token, :string
+  end
+end
