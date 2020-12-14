f50e1cfd41fe14be727bd7a6aa120c265f09ffaa
SVRourke <samuel@rourke.tech>
2020-12-14 09:40:54 -0500

    fixed bug with quiz creation validation

--- app/validators/quiz_validator.rb
+++ app/validators/quiz_validator.rb
@@ -1,6 +1,6 @@
 class QuizValidator < ActiveModel::Validator
     def validate(record)
-        if record.group.quizzes.any? {|q| q.name == record.name}
+        unless record.group.quizzes.none? {|q| q.name == record.name}
             record.errors[:quiz] << "Quiz already exists for #{record.group.name}"
         end
     end
