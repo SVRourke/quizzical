862d04824f8345f9b18c569c3094482519cb2fa0
SVR <samuelvanrourke@gmail.com>
2020-12-24 12:31:35 -0500

    added initializer to remove unneeded activestorage routes

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,9 @@ module Quizzical
 
     config.action_dispatch.rescue_responses["Pundit::NotAuthorizedError"] = :forbidden
 
+    initializer(:remove_activestorage_routes, after: :add_routing_paths) {|app|
+      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}}
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
